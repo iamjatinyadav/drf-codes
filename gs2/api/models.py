6cec33e485f1e3ff285bd7a03efd692317d78e26
iamjatinyadav <jatinyadav0000@gmail.com>
2022-11-21 09:40:45 +0530

    make changes

---
+++ gs2/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll = models.IntegerField()
+    city = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['id']
+        db_table = 'Student'
+        # managed = True
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+
+    def __str__(self) -> str:
+        return self.name
---
+++ gs2/api/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import student_create
+urlpatterns = [
+    path('', student_create),
+]
