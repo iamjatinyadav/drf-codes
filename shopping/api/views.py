ca9fe4caf18669ca322d9711936165d9e726bffd
iamjatinyadav <jatinyadav0000@gmail.com>
2022-11-30 11:54:51 +0530

    add retrive method in cheap and recent product

--- shopping/api/views.py
+++ shopping/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from rest_framework.views import APIView
-
+from rest_framework.status import *
 # Create your views here.
 
 class ProductListView(viewsets.ModelViewSet):
@@ -25,6 +25,33 @@ class ProductListView(viewsets.ModelViewSet):
         post = Product.objects.order_by('discount_price')[:6]
         serializer = ProductsSerializers(post, many=True, context = {'request': request})
         return Response(serializer.data)
+    
+    @action(detail=False, methods=["GET"], url_path="cheap-product/(?P<pk>\d+)")
+    def cheap_product_detail(self,request, pk=None):
+        id = int(self.kwargs['pk'])
+        post = list(Product.objects.order_by('discount_price')[:6].values_list("id", flat=True))
+        if id in post:
+            post = Product.objects.get(pk=id)
+            serializer = ProductsSerializers(post, context = {'request': request})
+            return Response(serializer.data)
+        else:
+            return Response(status=HTTP_404_NOT_FOUND)
+
+
+
+    @action(detail=False, methods=["GET"], url_path="recent-product/(?P<pk>\d+)")
+    def recent_product_detail(self,request, pk=None):
+        id = int(self.kwargs['pk'])
+        post = list(Product.objects.order_by('-id')[:6].values_list("id", flat=True))
+        print(post)
+        if id in post:
+            post = Product.objects.get(pk=id)
+            serializer = ProductsSerializers(post, context = {'request': request})
+            return Response(serializer.data)
+        else:
+            return Response(status=HTTP_404_NOT_FOUND)
+    
+
 
 
 
